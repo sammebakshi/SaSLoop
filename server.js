c9fe2517cee1beed9f0f35e329149828d10b0781
Sajad Bakshi <sajad@example.com>
2026-07-22 21:14:29 +0530

    Add root build directory to candidatePaths in server.js

--- server.js
+++ server.js
@@ -205,6 +205,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"), {
 // ======================
 // Resolve build path relative to server.js
 let candidatePaths = [
+    path.resolve(__dirname, "build"),
     path.resolve(__dirname, "SaSLoop-dashboard", "build"),
     path.resolve(__dirname, "SaSLoop-dashboard", "dist"),
     path.resolve(__dirname, "..", "SaSLoop-dashboard", "build"),
